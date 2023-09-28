694ce4b83282c328443f5f3efaee5c6a814345fe
eskil <eskiltb@hotmail.com>
2023-09-28 19:21:21 +0200

    Can search for person by last or first name

--- pages/peoplePages/addPerson.jsx
+++ pages/peoplePages/addPerson.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export function AddPerson() {
+
+    const [firstName, newFirstName] = useState("");
+    const [lastName, newLastName] = useState("");
+    const [age, newAge] = useState(0);
+
     return (
         <div>
             <h1>Add Person</h1>
--- pages/peoplePages/findPerson.jsx
+++ pages/peoplePages/findPerson.jsx
@@ -1,7 +1,81 @@
+import React, { useState } from "react";
+import { fetchJSON } from "../../tools/FetchJSON";
+
+export function PersonCard ({ person }) {
+    const { firstName, lastName, age } = person;
+    return (
+      <div>
+        <p>
+          {firstName} {lastName}, {age}
+        </p>
+      </div>
+    );
+}
+
+const firstNameContext = React.createContext({
+    async searchByFirstName( variable ) {
+      return await fetchJSON(`/api/people/search/first/${variable}`);
+    },
+  });
+
+  const lastNameContext = React.createContext({
+    async searchByLastName( variable ) {
+        return await fetchJSON(`/api/people/search/last/${variable}`)
+    },
+  });
+
 export function FindPerson() {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    const [person, setPerson] = useState(null);
+
+    async function searchByFirstName(firstName){
+        const response = await fetchJSON(`/api/people/search/first/${firstName}`)
+        setPerson(response);
+    }
+
+    async function searchByLastName(lastName) {
+        const response = await fetchJSON(`/api/people/search/last/${lastName}`);
+        setPerson(response);
+    }
+
+    function handleSubmitFirst(e) {
+        e.preventDefault();
+        searchByFirstName(firstName);
+      }
+    
+      function handleSubmitLast(e) {
+        e.preventDefault();
+        searchByLastName(lastName);
+      }
+
     return (
         <div>
             <h1>Search for Person</h1>
+
+            <h4>Search by first name:</h4>
+            <form onSubmit={handleSubmitFirst}>
+                <input
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+
+            <h4>Search by last name:</h4>
+            <form onSubmit={handleSubmitLast}>
+                <input
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+
+            <p>Result:</p>
+
+            {person ? <PersonCard person={person} /> : <p>No results</p>}
 
         </div>
     );
